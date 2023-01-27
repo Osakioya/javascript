6b5c872f98f72ab0b28a25a4e9958e84a28d9954
osakioya <ugiagbeosakioyaosakioya@yahoo.co.uk>
2023-01-27 12:58:06 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const person = {
+    playername: "leo";
+    goalsScored: 4,
+    gamesPlayer: 2,
+};
+cons.log(playerone);
+
+    cons tNmae = {
+        teamName: "Lion:",
+        numberplayers = 18,
+        varsity: true
+    }
+
+    console:log(teams);
